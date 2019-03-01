7bef1ba15ef0403d7bf2cf6401533b3802b0c78a
David <dajohnson1998@gmail.com>
2019-03-01 12:01:51 -0500

    added helper functions peek, display, push, pop

--- stack.js
+++ stack.js
@@ -4,4 +4,31 @@ class Stack {
   constructor() {
     this.top = null;
   }
+
+  pop() {
+    const node = this.top;
+    this.top = node.next
+    return node.data;
+  }
+
+  push(data) {
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
+    }
+
+    const node = new _Node(data, this.top)
+    this.top = node;
+  }
 }
+
+function peek(stack) {
+  return stack.top
+}
+
+function display(stack) {
+  console.log(JSON.stringify(stack, null, 2))
+}
+
+
+module.exports = {Stack, peek, display}
